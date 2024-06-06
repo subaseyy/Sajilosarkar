b476cdbf4243f4c7ccf156e9f0765cc3062e8579
Subas Kandel <69797726+subasoffl@users.noreply.github.com>
2024-06-07 00:27:06 +0545

    frond end initialize and added tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='flex justify-center bg-black text-white items-center h-96 text-3xl font-serif '>Sajilo Sarkar</h1>
+    </>
+  )
+}
+
+export default App
